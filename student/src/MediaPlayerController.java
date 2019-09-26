f5c30cd84881dbaee66b35628112c8f192ca0fca
samhogan <shluamobilegames@gmail.com>
2019-09-26 12:08:36 -0400

    mediaPlayer correclty scales and positions when the window is resized

--- student/src/MediaPlayerController.java
+++ student/src/MediaPlayerController.java
@@ -53,6 +53,9 @@ public class MediaPlayerController
     @FXML
     BorderPane borderPane;
 
+    @FXML
+    HBox toolBarHBox;
+
     Duration duration;
     int flag=0;
     double prev;
@@ -96,6 +99,9 @@ public class MediaPlayerController
         timeSlider.valueProperty().addListener((Observable ov) -> onTimeSliderChange(ov));
         speedSlider.valueProperty().addListener((Observable ov) -> onSpeedSliderChange(ov));
 
+        mediaView.fitWidthProperty().bind(borderPane.widthProperty());
+        mediaView.fitHeightProperty().bind(borderPane.heightProperty().subtract(40));
+        toolBarHBox.prefWidthProperty().bind(borderPane.widthProperty());
 
 
     }
