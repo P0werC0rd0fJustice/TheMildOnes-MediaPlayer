d43a6c0a86cf950b5aead569b2a4679fe0eee9db
samhogan <shluamobilegames@gmail.com>
2019-10-26 14:20:21 -0400

    added unit tests for file copying

---
+++ student/test/FileEditorTest.java
@@ -0,0 +1,76 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class FileEditorTest
+{
+    FileEditor editor;
+    File fileCopy;
+
+    @Before
+    public void createEditorObject()
+    {
+        editor = new FileEditor();
+    }
+
+    @Test
+    public void copyCurrentFile_validFileAndNewFileName_createFileCopy()
+    {
+        File videoFile = new File("student/test/videos/vid1.mp4");
+        editor.setCurrentFile(videoFile);
+        boolean didCopy = editor.copyCurrentFile("filecopy.mp4");
+
+        //correct return value
+        assertTrue(didCopy);
+
+        //check if actually copied
+        fileCopy = new File("student/test/videos/filecopy.mp4");
+        assertTrue(fileCopy.exists());
+    }
+
+    @Test
+    public void copyCurrentFile_existingFileName_doNotCopyFile()
+    {
+        File videoFile = new File("student/test/videos/vid1.mp4");
+        editor.setCurrentFile(videoFile);
+        boolean didCopy = editor.copyCurrentFile("vid3.mp4");//existing file name
+
+        //should be false because copy failed
+        assertFalse(didCopy);
+    }
+
+    @Test
+    public void copyCurrentFile_invalidFile_doNotCopyFile()
+    {
+        //this file does not exist
+        File videoFile = new File("student/test/videos/notafile.mp4");
+        editor.setCurrentFile(videoFile);
+        boolean didCopy = editor.copyCurrentFile("filecopy.mp4");
+
+        //copy should have not occured
+        assertFalse(didCopy);
+    }
+
+    @After
+    public void deleteCreatedFiles()
+    {
+        if(fileCopy == null)
+            return;
+
+        try
+        {
+            Files.deleteIfExists(fileCopy.toPath());
+        }
+        catch(IOException e)
+        {
+
+        }
+    }
+}
