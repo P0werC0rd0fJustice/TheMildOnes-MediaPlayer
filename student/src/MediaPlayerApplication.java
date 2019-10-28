5c5cadd37409502513ea61cfbe2108ee95a6d66a
egrace901 <50183712+egrace901@users.noreply.github.com>
2019-10-28 16:03:08 -0400

    Application starts at an appropriate size

--- student/src/MediaPlayerApplication.java
+++ student/src/MediaPlayerApplication.java
@@ -5,6 +5,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 
 public class MediaPlayerApplication extends Application {
 
@@ -18,6 +20,11 @@ public class MediaPlayerApplication extends Application {
         primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.getIcons().add(new Image("windowImages/media_player_icon_by_xylomon.png"));
         primaryStage.setMinWidth(700.0);
+
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        primaryStage.setWidth(primaryScreenBounds.getWidth() - (primaryScreenBounds.getWidth()/4));
+        primaryStage.setHeight(primaryScreenBounds.getHeight() - (primaryScreenBounds.getHeight()/10));
+
         primaryStage.show();
 
         controller.setStage(primaryStage);
